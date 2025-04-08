9a31404ff3f7dfb9347516d5cfba16924884aaa3
Martin Gonzales <u202319724@upc.edu.pe>
2025-04-08 17:47:12 -0500

    correcion.

--- ACME.OOP/Procurement/Domain/Model/Aggregates/PurchaseOrderItem.cs
+++ ACME.OOP/Procurement/Domain/Model/Aggregates/PurchaseOrderItem.cs
@@ -6,7 +6,7 @@ namespace ACME.OOP.Procurement.Domain.Model.Aggregates;
 public class PurchaseOrderItem(ProductId productId, int quatity, Money unitPrice)
 {
     public ProductId ProductId { get;} = productId ?? throw new ArgumentNullException(nameof(productId));
-    public int Quantity { get; } = quatity > 0 ? throw new ArgumentOutOfRangeException(nameof(quatity)) : quatity;
+    public int Quantity { get; } = quatity > 0 ? quatity: throw new ArgumentOutOfRangeException(nameof(quatity));
     public Money UnitPrice { get; } = unitPrice ?? throw new ArgumentNullException(nameof(unitPrice));
 
     public Money CalculateSubtotal() => new(amount: Quantity * unitPrice.Amount, UnitPrice.Currency);
